536da6c26cb09b903d39d7e89ccdfeee36a64cc5
Athreya Murali <34428511+DevByAthi@users.noreply.github.com>
2021-04-23 01:52:08 -0400

    refactored code by splitting message sending into separate sensor and actuator files

--- devices/dummy_drone_sim.py
+++ devices/dummy_drone_sim.py
@@ -12,7 +12,7 @@ import dotenv
 # The sample connects to a device-specific MQTT endpoint on your IoT Hub.
 from azure.iot.device import IoTHubDeviceClient, Message
 
-from util.mocking_util import generate_sensor_message_text
+from util.mocking_util import generate_actuator_message_text
 
 config = dotenv.dotenv_values()
 
@@ -21,6 +21,8 @@ DEVICE_CONNECTION_STRING = config['DEVICE_CONNECTION_STRING']
 
 # Define the JSON message to send to IoT Hub.
 TEMPERATURE = 20.0
+BAROMETRIC_PRESSURE = 1013.25
+ALTITUDE = 50
 HUMIDITY = 60
 CARBON_MONOXIDE = 10
 MSG_TXT = '{{"type": "Feature", "geometry" : {{"type" : "Point", "coordinates" : {coords}}}, "properties" : {{ "device_type" : "{device_type}", "temperature" : {{"value" : "{temperature}", "unit" : "celcius"}}, "humidity" : {{"value" : "{humidity}", "unit" : "celcius"}}, "CO" : {{"value" : "{carbon_monoxide}", "unit" : "ppm"}}}}}}'
@@ -44,11 +46,13 @@ def iothub_client_telemetry_sample_run():
             # Build the message with simulated telemetry values.
             lat, long = 28.5, -34.7
             temperature = TEMPERATURE + (random.random() * 15)
-            humidity = HUMIDITY + (random.random() * 20)
-            carbon_monoxide = CARBON_MONOXIDE + ((random.random() - 0.5) * 10)
-            particulates = (random.random() * 10)
+            baro = BAROMETRIC_PRESSURE + ((random.random() - 0.5) * 60)
+            altitude = ALTITUDE + ((random.random() - 0.5) * 12)
+            acc = (random.random() * 2.0, random.random() * 2.0, random.random() * 2.0)
+            gyro = (random.random() * 2.0, random.random() * 2.0, random.random() * 2.0)
+            mag = (random.random() * 2.0, random.random() * 2.0, random.random() * 2.0)
 
-            msg_txt_formatted = generate_sensor_message_text(lat, long, temperature, humidity, carbon_monoxide, particulates)
+            msg_txt_formatted = generate_actuator_message_text(lat, long, acc, gyro, mag, baro, altitude, temperature)
             message = Message(msg_txt_formatted)
 
             # Add a custom application property to the message.
